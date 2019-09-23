9fa12eaabda33e7ab6df874ad5bb80f54cab8afc
dardo landa <dardolanda@dardos-MacBook-Pro.local>
2019-09-23 15:41:43 -0300

    Cancel order

--- src/main/groovy/bff/bridge/http/OrderBridgeImpl.groovy
+++ src/main/groovy/bff/bridge/http/OrderBridgeImpl.groovy
@@ -96,6 +96,7 @@ class OrderBridgeImpl implements OrderBridge {
         customerOrderResponse.accessToken = findSupplierOrderInput.accessToken
         customerOrderResponse.supplierOrder.accessToken = findSupplierOrderInput.accessToken
         customerOrderResponse.supplierOrder.order.accessToken = findSupplierOrderInput.accessToken
+        customerOrderResponse.supplierOrder.order.id = findSupplierOrderInput.orderId
         customerOrderResponse.customer.accessToken = findSupplierOrderInput.accessToken
 
         customerOrderResponse
