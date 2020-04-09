12e229c051b4e3fbc5c4930fb08897d4483cfd84
Conejo <lucianorusso@gmail.com>
2020-04-09 16:56:29 -0300

    SearchV2 with error handling

--- src/main/groovy/bff/bridge/SearchBridge.groovy
+++ src/main/groovy/bff/bridge/SearchBridge.groovy
@@ -1,9 +1,12 @@
 package bff.bridge
 
 import bff.model.SearchInput
+import bff.model.SearchResponse
 import bff.model.SearchResult
 
 interface SearchBridge {
 
     SearchResult search(SearchInput searchInput)
+
+    SearchResponse searchV2(SearchInput searchInput)
 }
