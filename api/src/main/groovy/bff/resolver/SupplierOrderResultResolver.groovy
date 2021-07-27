a91eb946e8658c3c99b425b3f231ec26c96e80e0
dlanda <dlanda@yopapp.me>
2021-07-27 09:07:00 +0000

    Squashed commit of the following
    commit 4c8b503f 
    Author: Dardo Landa <dlanda@wabidev.com> 
    Date: Thu Jul 22 2021 16:18:32 GMT-0300 (hora estándar de Argentina) 
    
        supplier order result resolver: get supplier rating

---
+++ api/src/main/groovy/bff/resolver/SupplierOrderResultResolver.groovy
@@ -0,0 +1,32 @@
+package bff.resolver
+
+import bff.bridge.SupplierOrderBridge
+import bff.model.RatingEntry
+import bff.model.RatingOwner
+import bff.model.SupplierOrderResult
+import com.coxautodev.graphql.tools.GraphQLResolver
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component
+class SupplierOrderResultResolver implements GraphQLResolver<SupplierOrderResult> {
+
+    @Autowired
+    SupplierOrderBridge supplierOrderBridge
+
+    RatingEntry rating(SupplierOrderResult supplierOrderRes) {
+        if (!supplierOrderRes.rating && supplierOrderRes.ratings && supplierOrderRes.ratings.size() > 0) {
+            def supplierRating = supplierOrderRes.ratings.get(RatingOwner.SUPPLIER)
+            supplierRating?.accessToken = supplierOrderRes.accessToken
+
+            def customerRating = supplierOrderRes.ratings.get(RatingOwner.CUSTOMER)
+            customerRating?.accessToken = supplierOrderRes.accessToken
+
+            return new RatingEntry(
+                    SUPPLIER: supplierRating,
+                    CUSTOMER: customerRating
+            )
+        }
+        supplierOrderRes.rating?:supplierOrderBridge.getRatingBySupplierOrderId(supplierOrderRes.accessToken, supplierOrderRes.id)
+    }
+}
