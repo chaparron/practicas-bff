7e98dbc605beda1f55b516728af2466527cf4a9e
Patricio Gorin <pgorin@yopapp.me>
2021-11-29 18:12:44 +0100

    Started using grocery listing when fetching product prices (lazy resolver).

--- api/src/main/groovy/bff/resolver/ProductResolver.groovy
+++ api/src/main/groovy/bff/resolver/ProductResolver.groovy
@@ -1,22 +1,28 @@
 package bff.resolver
 
 import bff.bridge.ProductBridge
-import bff.bridge.RecommendedOrderBridge
+import bff.bridge.sdk.GroceryListing
 import bff.configuration.BadRequestErrorException
 import bff.configuration.EntityNotFoundException
 import bff.model.*
 import com.coxautodev.graphql.tools.GraphQLResolver
+import graphql.schema.DataFetchingEnvironment
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+
+import static bff.support.DataFetchingEnvironments.experimentalMode
+import static java.util.Optional.ofNullable
 
 @Component
 class ProductResolver implements GraphQLResolver<Product> {
 
     @Autowired
     ProductBridge productBridge
-
     @Autowired
-    RecommendedOrderBridge recommendedOrderBridge
+    GroceryListing groceryListing
+    @Value('${grocery.listing.countries:}')
+    List<String> groceryListingEnabledCountries
 
     Category category(Product product) {
         productBridge.getCategoryByProductId(product.accessToken, product.id)
@@ -38,63 +44,53 @@ class ProductResolver implements GraphQLResolver<Product> {
         productBridge.getManufacturerByProductId(product.accessToken, product.id)
     }
 
-    List<Price> prices(Product product) {
+    List<Price> prices(Product product, DataFetchingEnvironment dfe) {
         try {
             product.prices
                     ? product.prices
-                    : productBridge.getPricesByProductId(product.accessToken, product.id)
+                    : getPrices(product, dfe)
         }
         catch (Exception ex) {
-            product.prices = null
+            product.prices = []
         }
     }
 
-    Price minUnitsPrice(Product product) {
-
+    Price minUnitsPrice(Product product, DataFetchingEnvironment dfe) {
         if (product.minUnitsPrice) {
             return product.minUnitsPrice
-        } else if (!product.prices) {
-            try {
-                product.prices = productBridge.getPricesByProductId(product.accessToken, product.id)
-            }
-            catch (EntityNotFoundException ex) {
-                product.prices = null
-            }
-            catch(BadRequestErrorException ex) { // Maybe no supplier price found
-                product.prices = null
+        } else {
+            product.prices = getPrices(product, dfe)
+            return product.prices.min { Price a, Price b ->
+                (a.minUnits == b.minUnits) ? a.unitValue <=> b.unitValue : a.minUnits <=> b.minUnits
             }
         }
-
-
-        def p = product.prices
-        if (p && !p.isEmpty()) {
-            Price minPriceUnits = p.min { it.minUnits }
-            List<Price> shared = p.findAll { it.minUnits == minPriceUnits.minUnits }
-            return shared.min { it.value }
-        }
-        null
     }
 
-    Price priceFrom(Product product) {
+    Price priceFrom(Product product, DataFetchingEnvironment dfe) {
         if (product.priceFrom) {
             return product.priceFrom
-        } else if (!product.prices) {
-            try {
-                product.prices = productBridge.getPricesByProductId(product.accessToken, product.id)
-            }
-            catch (EntityNotFoundException ex) {
-                product.prices = null
-            }
-            catch(BadRequestErrorException ex) { // Maybe no supplier price found
-                product.prices = null
-            }
+        } else {
+            product.prices = getPrices(product, dfe)
+            return product.prices.min { it.value }
         }
-        def p = product.prices
-        if (p && !p.isEmpty()) {
-            return p.min { it.value }
-        }
-        null
+    }
 
+    private List<Price> getPrices(Product product, DataFetchingEnvironment dfe) {
+        try {
+            return isGroceryListingEnabled(dfe, { product.country_id })
+                    ? groceryListing.getProductById(product.accessToken, product.id.toInteger()).prices
+                    : ofNullable(productBridge.getPricesByProductId(product.accessToken, product.id)).orElse([])
+        }
+        catch (EntityNotFoundException ex) {
+            return []
+        }
+        catch (BadRequestErrorException ex) { // Maybe no supplier price found
+            return []
+        }
+    }
+
+    private def isGroceryListingEnabled(DataFetchingEnvironment dfe, Closure<String> country) {
+        (experimentalMode(dfe) || ofNullable(groceryListingEnabledCountries).orElse([]).contains(country()))
     }
 
 }
