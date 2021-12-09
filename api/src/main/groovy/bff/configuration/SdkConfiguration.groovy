8cb38c56c1bfc536c6dcdb5f267124029884eb32
Unai Garcia <ugarcia@yopapp.me>
2021-12-09 17:14:21 +0000

    Squashed commit of the following
    commit c4b421cc 
    Author: Unai Garcia <ugarcia@yopapp.me> 
    Date: Fri Dec 03 2021 10:12:39 GMT+0100 (hora estándar de Europa central) 
    
        Add Mis creditos section

--- api/src/main/groovy/bff/configuration/SdkConfiguration.groovy
+++ api/src/main/groovy/bff/configuration/SdkConfiguration.groovy
@@ -2,18 +2,22 @@ package bff.configuration
 
 import bff.bridge.CustomerBridge
 import bff.bridge.sdk.GroceryListing
+import bff.bridge.sdk.credits.HttpCreditService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestOperations
 import wabi2b.grocery.listing.sdk.Sdk
+import wabi2b.sdk.credits.HttpSupplierCreditsSdk
 
 @Configuration
 class SdkConfiguration {
 
     @Value('${grocery.listing.endpoint:}')
     String groceryListingEndpoint
+    @Value('${supplier.credits.endpoint:}')
+    String creditsEndpoint
     @Autowired
     CustomerBridge customerBridge
     @Autowired
@@ -24,6 +28,13 @@ class SdkConfiguration {
         new GroceryListing(
                 sdk: new Sdk(client, groceryListingEndpoint.toURI()),
                 customerBridge: customerBridge
+        )
+    }
+
+    @Bean
+    HttpCreditService creditService(){
+        new HttpCreditService(
+                creditsSdk: new HttpSupplierCreditsSdk.Builder().withBaseURI(creditsEndpoint.toURI()).build()
         )
     }
 
