b75d60fabe0e4ea44c0d0dde4c78dd6b8ecb3d6c
Emilio López <elopez@yopapp.me>
2022-01-03 09:44:27 +0100

    Complete accessToken to summary

--- api/src/main/groovy/bff/bridge/http/OrderBridgeImpl.groovy
+++ api/src/main/groovy/bff/bridge/http/OrderBridgeImpl.groovy
@@ -289,6 +289,7 @@ class OrderBridgeImpl implements OrderBridge {
                             value: sm.meta.get(key)
                     )
                 }
+                sm.accessToken = accessToken
             }
         }
 
