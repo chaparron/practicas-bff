c93d7514eafa926d987134c857bfb60c53df8425
Patricio Gorin <pgorin@yopapp.me>
2022-05-06 16:23:08 +0200

    Fixed supplier resolver when fetching average delivery days on sync cart query.

--- api/src/main/groovy/bff/resolver/SupplierResolver.groovy
+++ api/src/main/groovy/bff/resolver/SupplierResolver.groovy
@@ -1,18 +1,15 @@
 package bff.resolver
 
 import bff.bridge.SupplierBridge
-import bff.model.PreviewSupplier
 import bff.model.Supplier
 import bff.model.SupplierAvatarSize
-import bff.model.utils.DfeUtils
 import com.coxautodev.graphql.tools.GraphQLResolver
 import graphql.schema.DataFetchingEnvironment
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 
+import static bff.model.utils.DfeUtils.isOperation
 import static java.util.Optional.ofNullable
-import static java.util.Optional.ofNullable
-
 
 @Component
 class SupplierResolver implements GraphQLResolver<Supplier> {
@@ -21,6 +18,7 @@ class SupplierResolver implements GraphQLResolver<Supplier> {
     SupplierBridge supplierBridge
 
     private final String REFRESH_CART_QRY = "refreshCart"
+    private final String SYNC_CART_QRY = "syncCart"
 
     String avatar(Supplier supplier, SupplierAvatarSize size) {
         ofNullable(supplier.avatar)
@@ -32,7 +30,8 @@ class SupplierResolver implements GraphQLResolver<Supplier> {
 
     String averageDeliveryDay(Supplier supplier, DataFetchingEnvironment dfe) {
         String accessToken = supplier.accessToken
-        return supplier.averageDeliveryDay || !DfeUtils.isOperation(dfe, REFRESH_CART_QRY ) ?
+        return supplier.averageDeliveryDay ||
+                !(isOperation(dfe, REFRESH_CART_QRY) || isOperation(dfe, SYNC_CART_QRY)) ?
                 supplier.averageDeliveryDay
                 : supplierBridge.getAverageDeliveryDays(accessToken, supplier.id)
     }
