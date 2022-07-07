5d43e24b79e2c51479b91bdd2cfa3420f26fe7c3
Dardo Landa <dlanda@wabidev.com>
2022-07-07 10:33:31 -0300

    profile sections, Invoices mock -> country check

--- api/src/main/groovy/bff/resolver/CustomerResolver.groovy
+++ api/src/main/groovy/bff/resolver/CustomerResolver.groovy
@@ -35,9 +35,8 @@ class CustomerResolver implements GraphQLResolver<Customer> {
         List<ProfileSection> ps = new ArrayList<ProfileSection>()
         ps.push(new ProfileSection(id: "ORDERS"))
         ps.push(new ProfileSection(id: "SUGGESTED_ORDER"))
-        // TODO: check country
-        ps.push(new ProfileSection(id: "INVOICES")) // MOCK Invoices
         if (customer.country_id == 'in'){
+            ps.push(new ProfileSection(id: "INVOICES")) // MOCK Invoices
             ps.push(new ProfileSection(id: "CREDIT_LINES"))
         }
         ps.push(new ProfileSection(id: "STORE_INFORMATION"))
