c8f60acdcba1b61d22b49afbe74f890da250b454
Manuel Campo <manuel.campo@wabiproject.com>
2022-09-08 22:07:59 +0000

    Squashed commit of the following
    commit 3cadd15c 
    Author: Manuel Campo <manuel.campo@wabiproject.com> 
    Date: Thu Sep 08 2022 19:07:04 GMT-0300 (hora estándar de Argentina) 
    
        Fix string contact
    
    
    commit f35090cf 
    Author: Manuel Campo <manuel.campo@wabiproject.com> 
    Date: Thu Sep 08 2022 17:58:21 GMT-0300 (hora estándar de Argentina) 
    
        Fix tests
    
    
    commit 220bcd3c 
    Author: Manuel Campo <manuel.campo@wabiproject.com> 
    Date: Thu Sep 08 2022 17:54:05 GMT-0300 (hora estándar de Argentina) 
    
        Merge branch 'master' into feature/WM-1516-Translate-PaymentButton
    
    
    commit bb287f0f 
    Author: Manuel Campo <manuel.campo@wabiproject.com> 
    Date: Thu Sep 08 2022 17:49:01 GMT-0300 (hora estándar de Argentina) 
    
        Translate Payment Button
    
    
    commit a92ce282 
    Author: cmartin <cmartin@wabiproject.com> 
    Date: Thu Sep 08 2022 07:03:46 GMT-0300 (hora estándar de Argentina) 
    
        Refactor to improve if conditions
    
    
    
    commit e7c0ba38 
    Author: cmartin <cmartin@wabiproject.com> 
    Date: Thu Sep 08 2022 06:23:53 GMT-0300 (hora estándar de Argentina) 
    
        Fix test
    
    
    
    commit 79f59b9b 
    Author: cmartin <cmartin@wabiproject.com> 
    Date: Thu Sep 08 2022 05:23:14 GMT-0300 (hora estándar de Argentina) 
    
        Fix tests (WIP)
    
    
    
    commit 56b94bcb 
    Author: cmartin <cmartin@wabiproject.com> 
    Date: Thu Sep 08 2022 04:43:17 GMT-0300 (hora estándar de Argentina) 
    
        Delete unused import
    
    
    
    commit c9a32272 
    Author: Manuel Campo <manuel.campo@wabiproject.com> 
    Date: Wed Sep 07 2022 18:09:18 GMT-0300 (hora estándar de Argentina) 
    
        WIP
    
    
    commit 06acb8ab 
    Author: Manuel Campo <manuel.campo@wabiproject.com> 
    Date: Wed Sep 07 2022 17:54:01 GMT-0300 (hora estándar de Argentina) 
    
        Revert "[Merge pull request 9609: Fix: Reactive pipeline on SupplierOrderResolver#supportedPaymentProviders](https://us-west-2.console.aws.amazon.com/codesuite/codecommit/repositories/wabi2b-bff/pull-requests/9609/details?region=us-west-2)"
    
    This reverts commit 2d53af47c9c9044bab2d5a16272aa6bb67d79a92.

--- api/src/main/groovy/bff/resolver/SupplierOrderResolver.groovy
+++ api/src/main/groovy/bff/resolver/SupplierOrderResolver.groovy
@@ -9,7 +9,6 @@ import com.coxautodev.graphql.tools.GraphQLResolver
 import digitalpayments.sdk.model.Provider
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
-import reactor.core.publisher.Mono
 import wabi2b.payments.common.model.request.GetSupplierOrderPaymentRequest
 import wabi2b.payments.common.model.response.GetSupplierOrderPaymentResponse
 
@@ -192,6 +191,7 @@ class SupplierOrderResolver implements GraphQLResolver<SupplierOrder> {
 
 abstract class SimpleTextButtonBuilder {
 
+    final PAYMENT_BUTTON_PREFIX = "payment."
     protected GetSupplierOrderPaymentResponse response
     protected List<SupportedPaymentProvider> providers
 
@@ -211,7 +211,7 @@ abstract class SimpleTextButtonBuilder {
     abstract boolean isSupported()
 
     protected SimpleTextButton build() {
-        def textKey = isFull() ? PaymentStatus.TOTALLY_PAID.name() : paymentStatus().name()
+        def textKey = PAYMENT_BUTTON_PREFIX + (isFull() ? PaymentStatus.TOTALLY_PAID.name() : paymentStatus().name())
         return new SimpleTextButton(behavior(), textKey)
     }
 
