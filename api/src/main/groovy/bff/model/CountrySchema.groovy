20c5cff9bf376692a03b9f5a27de694029439da6
afrangella <afrangella@yopapp.me>
2022-10-03 14:15:27 -0300

    use object instead of primitive

--- api/src/main/groovy/bff/model/CountrySchema.groovy
+++ api/src/main/groovy/bff/model/CountrySchema.groovy
@@ -24,7 +24,7 @@ class Country {
     WabiPay wabiPay
     LegalDocumentInformation legalDocumentInformation
     Geolocation geolocation
-    boolean customerBranchesEnabled
+    Boolean customerBranchesEnabled
 }
 
 class Detail {
